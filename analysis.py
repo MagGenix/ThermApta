043fcce77f46c6e0d1bd85b501f2ef89563257f4
Nana Osei-Owusu <osenani777@gmail.com>
2023-03-04 23:15:54 +0000

    Finish Analysis

--- analysis.py
+++ analysis.py
@@ -1,1 +1,19 @@
-import Aptamer, MeltProfile, Sequence
+import Aptamer, MeltProfile, Sequence 
+
+def dna_to_rna(sequence=""):
+    for bases in sequence:
+        if bases == "A":
+            bases.replace(bases, "U")
+        elif bases == "C":
+            bases.replace(bases, "G")
+        elif bases == "G":
+            bases.replace(bases, "C")
+        else:
+            bases.replace(bases, "A")
+    return sequence 
+    
+def rna_to_seq(mrna=""):
+    return Sequence(mrna)
+
+def seq_to_aptamer(sequence=""):
+    return Aptamer(sequence)
