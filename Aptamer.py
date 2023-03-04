9b830973f9a058cb036594c5cec15e2a4996a87f
Nana Osei-Owusu <osenani777@gmail.com>
2023-03-04 20:36:34 +0000

    updating Aptamer

--- Aptamer.py
+++ Aptamer.py
@@ -1,3 +1,5 @@
+import Sequence
+import MeltProfile
 
 class Aptamer():
     def __init__(self, assembled_seq = [], structure = [], melt_prof = []): 
@@ -6,46 +8,45 @@ class Aptamer():
         self.structure = structure
         self.melt_prof = melt_profile 
         
-    def graph_points(): 
+    def graph_points(self): 
         #plots the melt profile 
         #Calls Method in Meltprofile Class 
         return self.melt_prof.plot()
         
-    def updateGC():
+    def update_bases(self):
         #Manually read through assembled sequences to count G and C content
         #O(N) - reads every character of every sequence in assembled_seq
-        #Saves information in fields G and C
-        '''WILL LIKELY CHANGE TO UPDATE A, U, G, and C'''
+        #Saves information in fields for all bases 
+        
         dna = self.assembeled_seq
-        count_c = 0 
-        count_g = 0
         total = []
         
         for sequence in dna:
-            for position in range(len(sequence)):
-                if sequence[position] == "C":
-                    count_c += 1
-                elif sequence[position] == "G":
-                    count_g += 1
-            count_dictionary = {"Number_of_C":count_c, "Number_of_G":count_g}
-            total.append(count_dictionary)
-        
+            Sequence.updateBases(sequence)
+            total.append([self.a, self.u, self.g, self.c])
+            
         return total
     
-    def getGC(sequence= ""):
+    def get_bases(self, sequence= ""):
         #Returns stored G/C values
         dna = self.assembled_seq
         ref = dna.index(sequence)
-        total = self.updateGC() 
-        '''Probably change into 4 getter functions for A, U, G, C'''
-        return [total[ref].values()]
+        total = self.update_bases() 
+        
+        return [total[ref]]
+    
+    def length(self, sequence=""):
+        bases = self.get_bases(sequence)
+    
+        return sum(bases)
+        
     
     
-    def isBlacklisted(blacklist_file=""):
+    def isBlacklisted(self, blacklist_file=""):
     # checks for primary sequence in the blacklist file and returns True if present 
     reference = self.assembled_seq
 
-    with open(blacklist_file) as f:
+    with open(self, blacklist_file) as f:
         sequences = f.readlines() # Assuming each sequence is on a newline 
 
         for i in reference:
@@ -55,7 +56,7 @@ class Aptamer():
                 else:
                     pass # For now
                 
-    def addBlacklisted(blacklist_file=""):
+    def addBlacklisted(self, blacklist_file=""):
     # checks for primary sequence in the blacklist file and adds sequence if not present 
     reference = self.assembled_seq
 
